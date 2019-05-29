f681ffdb4a9e48764b778f3176abdefbdd4b02a1
Chrispine Otaalo <john.otaalo@strathmore.edu>
2019-05-29 12:45:22 +0300

    Some samplecontroller

---
+++ app/Http/Controllers/Test/SampleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Test;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SampleController extends Controller
+{
+    function index(){
+    	echo "Here we are";
+    }
+}
