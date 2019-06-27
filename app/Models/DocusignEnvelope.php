9255c0335235eda7a13f3d8a511f64b3315ef73c
Chrispine Otaalo <otaaloc@UNNAIROBI.UN.ORG>
2019-06-27 15:09:30 +0300

    Sorted update issue

--- app/Models/DocusignEnvelope.php
+++ app/Models/DocusignEnvelope.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class DocusignEnvelope extends Model
 {
+	protected $primaryKey = "ID";
     protected $connection = "pm_data";
     protected $table = "pm_docusign_envelope";
     protected $fillable = ["CASE_NUMBER", "ENVELOPE_ID", "TASK", "PROCESS", "STATUS", "LAST_MODIFIED"];
