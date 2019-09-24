ea92979007397cdc8f5335eb678aa27387e934aa
Chrispine Otaalo <otaaloc@UNNAIROBI.UN.ORG>
2019-09-24 11:25:03 +0300

    Made blanket vat have decimal for amount

---
+++ database/migrations/2019_09_24_081918_vat_02_change_amount_to_varchar.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Vat02ChangeAmountToVarchar extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('pm_data')->table('VAT_02', function (Blueprint $table) {
+            $this->decimal('AMOUNT', 12, 2)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('pm_data')->table('VAT_02', function (Blueprint $table) {
+            $this->float('AMOUNT', 12, 2)->change();
+        });
+    }
+}
