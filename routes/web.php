850f5e84a3cc147baa023c99720aa7bb34af81e0
Chrispine Otaalo <otaaloc@UNNAIROBI.UN.ORG>
2019-09-27 09:32:32 +0300

    Added auth class in middleware

--- routes/web.php
+++ routes/web.php
@@ -16,27 +16,29 @@
 // });
 Route::get('/photos/principal/{host_country_id}', function($host_country_id){
 	$principal = \App\Models\Principal::where('HOST_COUNTRY_ID', $host_country_id)->first();
-	$filename = $principal->IMAGE;
+	if($principal){
+		$filename = $principal->IMAGE;
 
-	if (!\Storage::exists($filename)) {
-		// die("No file name");
-		// abort(404);
-		$file = public_path('images/no_avatar.svg');
-		$type = \File::mimeType($file);
+		if (!\Storage::exists($filename)) {
+			// die("No file name");
+			// abort(404);
+			$file = public_path('images/no_avatar.svg');
+			$type = \File::mimeType($file);
 
-		$headers = array('Content-Type: ' . $type);
+			$headers = array('Content-Type: ' . $type);
 
-		return Response::download($file, 'no_avatar.svg',$headers);
+			return Response::download($file, 'no_avatar.svg',$headers);
+		}
+
+		$file = \Storage::get($filename);
+		$type = \Storage::mimeType($filename);
+
+		$response = Response::make($file, 200);
+		$response->header("Content-Type", $type);
+
+		return $response;
 	}
-
-	$file = \Storage::get($filename);
-	$type = \Storage::mimeType($filename);
-
-	$response = Response::make($file, 200);
-	$response->header("Content-Type", $type);
-
-	return $response;
-})->middleware('isAdmin')->name('principal-photo');
+})->name('principal-photo');
 
 Route::get('/photos/dependent/{host_country_id}', function($host_country_id){
 	$dependent = \App\Models\PrincipalDependent::where('HOST_COUNTRY_ID', $host_country_id)->first();
