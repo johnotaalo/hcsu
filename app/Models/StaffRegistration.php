e11832d5bce76e2f240ae0b79c6bfd836c6003ec
Chrispine Otaalo <john.otaalo@strathmore.edu>
2019-11-18 10:12:19 +0300

    Added staff registartion table

---
+++ app/Models/StaffRegistration.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StaffRegistration extends Model
+{
+    protected $table = "AC_00";
+}
---
+++ database/migrations/2019_11_18_070422_create_staff_registrations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStaffRegistrationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('pm_data')->create('AC_00', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('CASE_NO');
+            $table->integer('HOST_COUNTRY_ID');
+            $table->integer('IPMIS_NO');
+            $table->integer('IPMIS_STATUS');
+            $table->text('IPMIS_COMMENT')->nullable();
+            $table->text('STAFF_REGISTRATION_LINK');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('AC_00');
+    }
+}
