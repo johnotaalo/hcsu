4a286bd2421b4ae619928293e754dace89feea46
Chrispine Otaalo <john.otaalo@strathmore.edu>
2020-04-01 14:21:00 +0300

    Added immigration data

---
+++ database/migrations/2020_04_01_110942_add_manager_approval_to_im_06.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddManagerApprovalToIm06 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('pm_data')->table('IM_06', function (Blueprint $table) {
+            $table->integer('MANAGER_APPROVAL')->after('NV_SERIAL')->nullable();
+            $table->longText('MANAGER_APPROVAL_COMMENT')->after('MANAGER_APPROVAL')->nullable();
+            $table->integer('SUBMIT_TO_IMMIGRATION')->after('MANAGER_APPROVAL_COMMENT')->nullable();
+            $table->longText('SUBMIT_TO_IMMIGRATION_COMMENT')->after('SUBMIT_TO_IMMIGRATION')->nullable();
+            $table->integer('IMMIGRATION_FEEDBACK')->after('SUBMIT_TO_IMMIGRATION_COMMENT')->nullable();
+            $table->integer('IMMIGRATION_FEEDBACK_COMMENT')->after('IMMIGRATION_FEEDBACK')->nullable();
+            $table->boolean('PASSPORT_SUBMITTED')->after('IMMIGRATION_FEEDBACK_COMMENT')->nullable();
+            $table->boolean('PASSPORT_RECEIVED')->after('PASSPORT_SUBMITTED')->nullable();
+            $table->boolean('PASSPORT_COLLECTED')->after('PASSPORT_RECEIVED')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('pm_data')->table('IM_06', function (Blueprint $table) {
+            $table->dropColumn();
+        });
+    }
+}
