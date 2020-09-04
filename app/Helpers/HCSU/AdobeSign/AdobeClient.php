0c496b13c0a464dc4a3c22ec3a787c9486fae76f
Chrispine Otaalo <john.otaalo@strathmore.edu>
2020-09-04 10:27:16 +0300

    Added data for unon

--- app/Helpers/HCSU/AdobeSign/AdobeClient.php
+++ app/Helpers/HCSU/AdobeSign/AdobeClient.php
@@ -90,7 +90,7 @@ class AdobeClient{
 				'documentCreationInfo'	=>	[
 					'recipientSetInfos'	=>	[
 						[
-							"recepientSetRole"				=>	"SIGNER",
+							"recipientSetRole"				=>	"SIGNER",
 							"recipientSetMemberInfos"		=>	$clientEmail
 						],
 						[
