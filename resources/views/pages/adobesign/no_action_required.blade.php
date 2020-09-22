31037577a0c11bae0b4bf710647488e8bbe985de
Chrispine Otaalo <john.otaalo@strathmore.edu>
2020-09-22 13:09:46 +0300

    Fixed some issues

--- resources/views/pages/adobesign/no_action_required.blade.php
+++ resources/views/pages/adobesign/no_action_required.blade.php
@@ -4,6 +4,7 @@
 <div class="text-center">
 	<center><img src="{{ asset('images/thumbs-up.png') }}" style="width: 100%;"></center>
 	<h1>No Action is required from you</h1>
-	<p>You are not allowed to view this page. Contact admin for more details</p>
+	<p>Looks like the document has already been signed or Adobe has not sent back an agreement id.</p>
+	<p>Contact the Administrator in case you have not signed the document yet.</p>
 </div>
 @endsection
