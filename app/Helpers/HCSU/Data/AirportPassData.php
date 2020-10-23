7489bef7977468ff714fb221d9bc8867760a8ae1
Chrispine Otaalo <john.otaalo@strathmore.edu>
2020-10-23 16:19:10 +0300

    Fixed data

--- app/Helpers/HCSU/Data/AirportPassData.php
+++ app/Helpers/HCSU/Data/AirportPassData.php
@@ -2,6 +2,37 @@
 
 namespace App\Helpers\HCSU\Data;
 
+use App\Models\AirportPass;
+
 class AirportPassData{
-	
+	public static function get($case_no, $type = "new"){
+		$data = new \StdClass;
+		$clientObj = new \StdClass;
+
+		$airportPassData = ($type == "new") ? AirportPass::where('CASE_NO', $case_no) : NULL;
+
+		$principal = \App\Models\Principal::where('HOST_COUNTRY_ID', $airportPassData->HOST_COUNTRY_ID)->first();
+		$contract = collect(\DB::select("CALL GET_LATEST_PRINCIPAL_CONTRACT({$dipidData->HOST_COUNTRY_ID})"))->first();
+
+		$mission = $contract->ACRONYM;
+
+		$name = format_other_names($principal->OTHER_NAMES) . " " . strtoupper($principal->LAST_NAME);
+		$client_name = $name;
+
+		$clientObj->name = $client_name;
+		$clientObj->designation = $contract->DESIGNATION;
+		$clientObj->organization = $mission;
+		$clientObj->contract_type = $contract->C_TYPE;
+		$clientObj->passport = ($principal->latest_passport) ? $principal->latest_passport->PASSPORT_NO : "N/A";
+		$clientObj->doa = ($principal->current_arrival) ? $principal->current_arrival->ARRIVAL : "N/A";
+		$clientObj->principal = $principal;
+
+		$data->client = $clientObj;
+		$data->case_no = $case_no;
+        $data->ref = $airportPassData->NV_SERIAL_NO;
+        $data->date = date('F d, Y');
+        $data->type = $type;
+
+		return $data;
+	}
 }
--- app/Helpers/HCSU/PDFTK/AirportPass.php
+++ app/Helpers/HCSU/PDFTK/AirportPass.php
@@ -15,5 +15,21 @@ class AirportPass{
 
 	public function getData($case_no, $document, $extraParams = null){
 		$form_data = \App\Helpers\HCSU\Data\AirportPassData::get($case_no);
+
+		$tabData = [
+			'serial_no'					=>	$form_data->ref,
+			'employee_name'				=>	$form_data->client->name,
+			'employee_organization'		=>	$form_data->client->organization,
+			'employee_postal_address'	=>	$form_data->client->principal->ADDRESS,
+			'employee_designation'		=>	$form_data->client->designation,
+			'duration'					=>	"1 Year",
+			'areas'						=>	'apron_lounges',
+			'identification'			=>	$form_data->client->passport,
+			'doa'						=>	$form_data->client->doa,
+			'r_number'					=>	($form_data->client->principal->R_NO != "" || $form_data->client->principal->R_NO != "null" || !is_null($form_data->client->principal->R_NO)) ? $form_data->client->principal->R_NO : "",
+			'applicant_date'			=>	$form_data->date
+		];
+
+		return $tabData;
 	}
 }
