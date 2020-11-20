749d13ae602ac36083312432d4d4e6d048dcba42
Chrispine Otaalo <john.otaalo@strathmore.edu>
2020-11-20 12:47:22 +0300

    Completed CRUD on processes c

--- app/Models/UserApplications.php
+++ app/Models/UserApplications.php
@@ -12,6 +12,6 @@ class UserApplications extends Model
    protected $fillable = ['PROCESS_UID','HOST_COUNTRY_ID','COMMENT','APP_UID','APP_NUMBER','ASSIGNED_TO','SUPERVISOR_COMMENTS','STATUS','SUBMITTED_BY','AUTHENTICATION_SOURCE','CURRENT_USER'];
 
 	public function process(){
-		return $this->belongsTo(\App\Models\PM\Process::class, 'PROCESS_UID', 'PRO_UID');
+		return $this->belongsTo(\App\Models\PM\Process::class, 'PRO_UID', 'PROCESS_UID');
 	}
 }
