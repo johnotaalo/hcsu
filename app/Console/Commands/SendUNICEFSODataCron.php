73bf66f55abb2af32190f4c106c78a4fadd14d21
Chrispine Otaalo <c.otaalo@gmail.com>
2021-01-04 17:43:23 +0300

    Added UNICEF-SO data dump

--- app/Console/Commands/SendUNICEFSODataCron.php
+++ app/Console/Commands/SendUNICEFSODataCron.php
@@ -3,26 +3,22 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Jobs\ExportOrganizationData;
-use Illuminate\Support\Facades\Mail;
 
-use \App\Jobs\SendOrganizationData;
-
-class SendOIOSIDDataCron extends Command
+class SendUNICEFSODataCron extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'senddata:oiosid';
+    protected $signature = 'senddata:unicef-so';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'This cron job sends data to OIOS ID daily';
+    protected $description = 'This cron job sends data to UNICEF(SO) daily';
 
     /**
      * Create a new command instance.
@@ -41,7 +37,7 @@ class SendOIOSIDDataCron extends Command
      */
     public function handle()
     {
-        $config = ((array)json_decode(\Storage::get('backup-settings.json')))['OIOSID'];
+        $config = ((array)json_decode(\Storage::get('backup-settings.json')))['UNICEF-SO'];
 
         $organizations = explode(',', $config->organizations);
         $recepients = explode(',', $config->recepients);
@@ -53,7 +49,7 @@ class SendOIOSIDDataCron extends Command
             return trim($recepient);
         })->toArray();
 
-        SendOrganizationData::dispatch($organizations, $recepients, "OIOS ID", "OIOS ID");
+        SendOrganizationData::dispatch($organizations, $recepients, "UNICEF (SO)", "UNICEF (SO)");
 
         \Log::info("Cron is working fine!");
 
