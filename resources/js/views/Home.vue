4f4d72e31e2f3ecc27a43f0f87ce07865e0dab7f
Chrispine Otaalo <c.otaalo@gmail.com>
2021-03-03 23:08:33 +0300

    Changed the Power BI Dashboard

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<b-card>
-			<iframe src="https://app.powerbi.com/view?r=eyJrIjoiYmQxMTlhZmItMzRiZS00NTlhLWE0ZTgtZmVkNGE5Y2MxODE2IiwidCI6IjBmOWUzNWRiLTU0NGYtNGY2MC1iZGNjLTVlYTQxNmU2ZGM3MCIsImMiOjh9" frameborder="0" allowFullScreen="true" style="width: 100%;height:100vh;"></iframe>
+			<iframe src="https://app.powerbi.com/reportEmbed?reportId=89294e1d-99d2-416f-9b2d-4ede3ae6c7c3&autoAuth=true&ctid=0f9e35db-544f-4f60-bdcc-5ea416e6dc70&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLW5vcnRoLWV1cm9wZS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameborder="0" allowFullScreen="true" style="width: 100%;height:100vh;"></iframe>
 		</b-card>
 	</div>
 </template>
