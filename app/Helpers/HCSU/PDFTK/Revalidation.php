80e0796c5e9a827a4f961bdc4b349cb0826a2dfe
Chrispine Otaalo <john.otaalo@strathmore.edu>
2021-04-14 15:35:28 +0300

    Completed fetching data from the new system to tabdata

--- app/Helpers/HCSU/PDFTK/Revalidation.php
+++ app/Helpers/HCSU/PDFTK/Revalidation.php
@@ -12,6 +12,23 @@ class Revalidation{
 	public function getData($case_no, $document){
 		$revalidationData = \App\Models\Revalidation::where('CASE_NO', $case_no)->first();
 
-		dd($revalidationData);
+		$tabData = [];
+
+		if ($revalidationData->INITIAL_SYSTEM == "new") {
+			if ($revalidationData->APPLICATION_TYPE == "pro-1a") {
+				$tabData = Pro1A::getData($revalidationData->INITIAL_CASE_NO, $document);
+			}else if ($revalidationData->APPLICATION_TYPE == "pro-1b") {
+				$clientType = identify_hcsu_client($revalidationData->HOST_COUNTRY_ID);
+				if($clientType == "agency"){
+					$tabData = Pro1BAgency::getData($revalidationData->INITIAL_CASE_NO, $document);
+				}else{
+					$tabData = Pro1BStaff::getData($revalidationData->INITIAL_CASE_NO, $document);
+				}
+			}else if ($revalidationData->APPLICATION_TYPE == "pro-1c") {
+				$tabData = Pro1C::getData($revalidationData->INITIAL_CASE_NO, $document);
+			}
+		}
+
+		return $tabData;
 	}
 }
